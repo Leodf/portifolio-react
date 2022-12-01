c9a1e9082d05cc94a5e126d0e70f2fb3f74e087b
Leonardo de Faveri <faver_i@hotmail.com>
2022-12-01 14:51:10 -0300

    finalizando a seção de portfolio

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { projectsNav } from './Data'
+import { projectsData } from './Data'
+import WorksItems from './WorksItems'
 
 const Works = () => {
+  const [item, setItem] = useState({ name: 'all'})
+  const [projects, setProjects] = useState([])
+  const [active, setActive] = useState(0)
+
+  useEffect(() => {
+    if(item.name === 'all') {
+      setProjects(projectsData)
+    }
+
+    else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name
+      })
+      setProjects(newProjects)
+    }
+  }, [item])
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent })
+    setActive(index)
+  }
   return (
-    <div>Works</div>
+    <div>
+      <div className="work-filters">
+      {projectsNav.map((item, index) => {
+        return (
+          <span 
+            onClick={(e) => {
+              handleClick(e, index)
+            }} 
+            className={`${active === index ? 'active-work' : ''} work-item`}
+            key={index}
+          >
+            {item.name}
+          </span>
+        )
+      })}
+      </div>
+
+      <div className="work-container container grid">
+        {projects.map((item) => {
+          return <WorksItems item={item} key={item.id}/>
+        })}
+      </div>
+    </div>
   )
 }
 
