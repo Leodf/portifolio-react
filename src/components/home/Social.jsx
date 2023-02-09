4980b5c28ea13162749438f52714d1c23916cb83
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-09 12:22:28 -0300

    feat: add links de redes sociais

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,17 +3,27 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home-social">
-        <a href="#https://www.instagram.com/" className="home-social-icon" target="_blank">
-            <i className='uil uil-instagram'></i>
-        </a>
 
-        <a href="#https://dribbble.com/" className="home-social-icon" target="_blank">
-            <i className='uil uil-dribbble'></i>
-        </a>
+      <a href="https://github.com/Leodf" rel="noreferrer" className="home-social-icon" target="_blank">
+        <i className='uil uil-github-alt'></i>
+      </a>
 
-        <a href="#https://github.com/" className="home-social-icon" target="_blank">
-            <i className='uil uil-github-alt'></i>
-        </a>
+      <a href="https://www.linkedin.com/in/leonardo-de-faveri/" className="home-social-icon" rel="noreferrer" target="_blank">
+        <i className='uil uil-linkedin'></i>
+      </a>
+
+      <a href="https://www.instagram.com/leodf41/" className="home-social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-instagram'></i>
+      </a>
+
+      <a href="https://www.facebook.com/leonardo.defaveri.9" className="home-social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-facebook'></i>
+      </a>
+
+      <a href="https://twitter.com/leonardo_faveri" className="home-social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-twitter'></i>
+      </a>
+
     </div>
   )
 }
